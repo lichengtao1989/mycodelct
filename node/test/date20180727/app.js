e2aac12fb92f02ee2581102479f57d9409d5ce8c
chengtao <lichengtao@app315.net>
2018-08-03 08:57:51 +0800

    node

--- node/test/date20180727/app.js
+++ node/test/date20180727/app.js
@@ -1,7 +1,24 @@
-var http=require('http');
+var http = require('http');
+var fs = require('fs');
 
-http.createServer(function(req,res){
+function read (files,options) {
+  console.log(files);
+  return new Promise((resolve, reject) => {
+    fs.readFile(files, options, (err,data)=>{
+      if(err) reject(err);
+      resolve(data)
+    })
+  })
+}
+http.createServer(function (req, res) {
   // res.writeHead({"content-type":"text/plain","status":200})
-  res.end('hello world');
+  res.setHeader("Content-Type", "text/html;chartset=UTF-8")
+  read('./test.html','utf8').then((data)=>{
+    // console.log(data);
+    res.end(data);
+  })
 
-}).listen(3600)
+  
+  
+
+}).listen(9900)

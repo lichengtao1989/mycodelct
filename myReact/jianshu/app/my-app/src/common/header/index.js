f37a66dd22c8a9365abb6f799e5f333e655d6fb4
chengtao <lichengtao@app315.net>
2018-10-08 11:19:13 +0800

    react

--- myReact/jianshu/app/my-app/src/common/header/index.js
+++ myReact/jianshu/app/my-app/src/common/header/index.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { 
   HeaderWrapper,
   Logo,
-  Nav
+  Nav,
+  NavItem,
+  NavSearch
  } from './style';
 class Header extends Component {
   render(){
     return (
       <HeaderWrapper>
         <Logo />
-        <Nav />
+        <Nav >
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载app</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>Aa</NavItem>
+        </Nav>
       </HeaderWrapper>
     )
   }
